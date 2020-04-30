5cef9573f8a473ac2f7e071168cb6042ddd77c1a
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2020-04-30 14:20:27 +0300

    Refactor scripts, enable auto-remove from PostgreSQL and pgAdmin containers

--- run-database.sh
+++ run-database.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 source variables.sh
 docker run -d --rm -p 5432:5432 \
 	-v $POSTGRES_VOLUME_PATH:/var/lib/postgresql/data \
--- run-pgadmin.sh
+++ run-pgadmin.sh
@@ -1,5 +1,6 @@
-sudo docker run -p 81:80 \
+#!/bin/bash
+docker run -d --rm -p 81:80 \
     -e 'PGADMIN_DEFAULT_EMAIL=user@domain.com' \
     -e 'PGADMIN_DEFAULT_PASSWORD=SuperSecret' \
-    -d dpage/pgadmin4
+    dpage/pgadmin4
 #--network="pgnetwork" \
