5ecf38b6a9e3803106ab9d9440e9f72f33876fd0
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2020-05-02 15:53:19 +0300

    Add comments on variables.sh

--- variables.sh
+++ variables.sh
@@ -1,5 +1,8 @@
+# The folder where all the other projects will be cloned to and stored
 export PROJECT_FOLDER=
+# The folder where PostgreSQL data will be stored
 export POSTGRES_VOLUME_PATH=
+# Projects, which are going to be cloned or built
 export PROJECTS='
 economics-client
 economics-server-products
